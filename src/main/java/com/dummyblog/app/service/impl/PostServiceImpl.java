5378b261fa12369d099b7f5b77326c276f5274ee
Maknyik Peter <maknyik97@gmail.com>
2021-04-30 14:29:05 +0200

    add UserRepository

--- src/main/java/com/dummyblog/app/service/impl/PostServiceImpl.java
+++ src/main/java/com/dummyblog/app/service/impl/PostServiceImpl.java
@@ -4,12 +4,18 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+import javax.annotation.PostConstruct;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dummyblog.app.domain.dto.PostDto;
 import com.dummyblog.app.repository.PostRepository;
+import com.dummyblog.app.repository.UserRepository;
+import com.dummyblog.app.repository.entity.Post;
+import com.dummyblog.app.repository.entity.User;
+import com.dummyblog.app.repository.entity.User.Privileges;
 import com.dummyblog.app.service.PostService;
 
 @Service
@@ -17,11 +23,13 @@ public class PostServiceImpl implements PostService {
 
 	private PostRepository postRepository;
 	private ModelMapper modelMapper;
+	private UserRepository userRepository;
 
 	@Autowired
-	public PostServiceImpl(PostRepository postRepository, ModelMapper modelMapper) {
+	public PostServiceImpl(PostRepository postRepository, ModelMapper modelMapper, UserRepository userRepository) {
 		this.postRepository = postRepository;
 		this.modelMapper = modelMapper;
+		this.userRepository = userRepository;
 	}
 
 	@Override
@@ -34,6 +42,19 @@ public class PostServiceImpl implements PostService {
 	public PostDto getPost(String title) {
 		Objects.requireNonNull(title, "Title is a required parameter for getting post.");
 		return modelMapper.map(postRepository.findByTitle(title), PostDto.class);
+	}
+//	Long id, Integer age, String email, String name, String password, Privileges role
+	@PostConstruct
+	public void init() {
+		User user = new User(null,3,"elso","1","1",Privileges.USER);
+		User user2 = new User(null,3,"masodik","2","2",Privileges.USER);
+		userRepository.save(user);
+		userRepository.save(user2);
+		
+		Post post = new Post("Főcím", "Alcím", "Ez itt a poszt tartalma.", user);
+		Post post2 = new Post("Főcím2", "Alcím2", "Ez itt a poszt tartalma.2", user2);
+		postRepository.save(post);
+		postRepository.save(post2);
 	}
 
 }
